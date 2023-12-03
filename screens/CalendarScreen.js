998b8660d5b1171f67a578d9546ce1921bc79071
Jungyu23 <yijungyu@naver.com>
2023-12-03 14:15:30 +0900

    운동 목록 확장, 오류상황 세분화, 중복 코드 삭제, 운동 선택 버그 여러 개 수정

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,14 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import { Alert, Button, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View, ToastAndroid, Platform } from 'react-native';
+import { Alert, Button, Modal, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View, ToastAndroid, Platform } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import RNPickerSelect from 'react-native-picker-select';
 
 // 운동 종류와 각 종류별 운동 목록을 정의합니다.
 const workoutTypes = {
-  전신: ['버피', '마운틴 클라이머', '점프 스쿼트', '플랭크', '하이 니즈', '스타 점프', '크로스 잭', '인치웜', '스케이터 스텝', '스파이더맨 플랭크'],
-  상체: ['푸시업', '트라이셉스 딥스', '플랭크 숄더 탭', '다이아몬드 푸시업', '슈퍼맨', '베어 크롤', '팔벌려 뛰기', '플랭크 업다운', '파이크 푸시업'],
-  하체: ['스쿼트', '런지', '글루트 브릿지', '카프 레이즈', '월 시트', '사이드 런지', '돈키 킥', '서클 레그 리프트', '수퍼맨 레그 리프트'],
+  전신: [
+    { name: '버피', timeBased: false },
+    { name: '마운틴 클라이머', timeBased: false },
+    { name: '점프 스쿼트', timeBased: false },
+    { name: '플랭크', timeBased: true },
+    { name: '하이 니즈', timeBased: true },
+    { name: '스타 점프', timeBased: false },
+    { name: '크로스 잭', timeBased: false },
+    { name: '인치웜', timeBased: false },
+    { name: '스케이터 스텝', timeBased: false },
+    { name: '스파이더맨 플랭크', timeBased: false },
+  ],
+  상체: [
+    { name: '푸시업', timeBased: false },
+    { name: '트라이셉스 딥스', timeBased: false },
+    { name: '플랭크 숄더 탭', timeBased: true },
+    { name: '다이아몬드 푸시업', timeBased: false },
+    { name: '슈퍼맨', timeBased: true },
+    { name: '베어 크롤', timeBased: true },
+    { name: '팔벌려 뛰기', timeBased: false },
+    { name: '플랭크 업다운', timeBased: false },
+    { name: '파이크 푸시업', timeBased: false },
+  ],
+  하체: [
+    { name: '스쿼트', timeBased: false },
+    { name: '런지', timeBased: false },
+    { name: '글루트 브릿지', timeBased: false },
+    { name: '카프 레이즈', timeBased: false },
+    { name: '월 시트', timeBased: true },
+    { name: '사이드 런지', timeBased: false },
+    { name: '돈키 킥', timeBased: false },
+    { name: '서클 레그 리프트', timeBased: false },
+    { name: '수퍼맨 레그 리프트', timeBased: false },
+  ],
 };
 
 const CalendarScreen = () => {
@@ -16,8 +47,10 @@ const CalendarScreen = () => {
   const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
   const [workouts, setWorkouts] = useState({});
   const [isModalVisible, setModalVisible] = useState(false);
-  const [workoutType, setWorkoutType] = useState(null); // 초기 운동 유형 설정
-  const [selectedWorkout, setSelectedWorkout] = useState(null); // 초기 선택된 운동 설정
+  const [workoutType, setWorkoutType] = useState(null); 
+  const [selectedWorkout, setSelectedWorkout] = useState(null);
+  const [timeBased, setTimeBased] = useState(false); 
+  const [duration, setDuration] = useState(''); 
   const [markedDates, setMarkedDates] = useState({});
   const [sets, setSets] = useState('');
   const [reps, setReps] = useState('');
@@ -30,7 +63,7 @@ const CalendarScreen = () => {
         if (jsonValue != null) {
           const storedWorkouts = JSON.parse(jsonValue);
           setWorkouts(storedWorkouts);
-          setMarkedDates(getMarkedDates(storedWorkouts)); // 마크된 날짜를 설정합니다.
+          setMarkedDates(getMarkedDates(storedWorkouts));
         }
       } catch (e) {
         console.error('Error loading data', e);
@@ -54,7 +87,7 @@ const CalendarScreen = () => {
     const marked = {};
     Object.keys(workouts).forEach(date => {
       if (workouts[date].length > 0) {
-        marked[date] = { marked: true, dotColor: 'blue' }; // 예시로 파란색 점을 사용
+        marked[date] = { marked: true, dotColor: 'blue' }; 
       }
     });
     return marked;
@@ -68,33 +101,54 @@ const CalendarScreen = () => {
 
   // 운동 정보를 저장하는 함수입니다.
   const handleSaveWorkoutDetail = () => {
-    // 세트와 횟수가 숫자인지, 그리고 1과 100 사이에 있는지 확인합니다.
     const setsInt = parseInt(sets, 10);
     const repsInt = parseInt(reps, 10);
-    const isSetsValid = setsInt >= 1 && setsInt <= 100;
-    const isRepsValid = repsInt >= 1 && repsInt <= 100;
-
-    if (!workoutType || !selectedWorkout || !isSetsValid || !isRepsValid) {
-      // 유효하지 않은 입력에 대한 오류 메시지를 출력합니다.
-      Alert.alert('오류', '모든 필드를 채워주세요. 세트와 횟수는 1부터 100 사이의 숫자여야 합니다.');
+    const durationInt = parseInt(duration, 10);
+    if (!workoutType) {
+      Alert.alert('오류', '운동 유형을 선택해주세요.');
       return;
     }
-
-    // 입력값이 모두 유효한 경우에만 저장 로직을 계속 진행합니다.
+    if (!selectedWorkout) {
+      Alert.alert('오류', '구체적인 운동을 선택해주세요.');
+      return;
+    }
+    if (timeBased) {
+      if (!(durationInt > 0 && durationInt <= 180)) {
+        Alert.alert('오류', '유효한 시간을 입력해주세요.(1~180초)');
+        return;
+      }
+    } else {
+      if (!(setsInt >= 1 && setsInt <= 100)) {
+        Alert.alert('오류', '세트는 1 이상 100 이하로 입력해주세요.');
+        return;
+      }
+  
+      if (!(repsInt >= 1 && repsInt <= 100)) {
+        Alert.alert('오류', '횟수는 1 이상 100 이하로 입력해주세요.');
+        return;
+      }
+    }
+    const workoutDetail = {
+      type: workoutType,
+      name: selectedWorkout.name,
+      timeBased: selectedWorkout.timeBased,
+      ...(timeBased ? { duration: durationInt } : { sets: setsInt, reps: repsInt }),
+    };
     const newWorkouts = {
       ...workouts,
       [selectedDate]: [
         ...(workouts[selectedDate] || []),
-        { type: workoutType, name: selectedWorkout, sets: setsInt, reps: repsInt }
+        workoutDetail
       ],
     };
     storeData(newWorkouts);
     setWorkouts(newWorkouts);
     setModalVisible(false);
-    setMarkedDates(getMarkedDates(newWorkouts)); // 운동 저장 후 마크된 날짜를 업데이트합니다.
+    setMarkedDates(getMarkedDates(newWorkouts));
     setSets('');
     setReps('');
-  };
+    setDuration('');
+  };  
 
   // 운동을 삭제할 때의 확인 대화 상자를 띄우는 함수입니다.
   const confirmDeleteWorkout = (dateString, index) => {
@@ -113,12 +167,12 @@ const CalendarScreen = () => {
     const updatedWorkouts = { ...workouts };
     updatedWorkouts[dateString].splice(index, 1);
     if (updatedWorkouts[dateString].length === 0) {
-      delete updatedWorkouts[dateString]; // 해당 날짜의 운동이 더 이상 없으면 키 삭제
+      delete updatedWorkouts[dateString];
     }
     setWorkouts(updatedWorkouts);
     storeData(updatedWorkouts);
-    setMarkedDates(getMarkedDates(updatedWorkouts)); // 운동 삭제 후 마크된 날짜를 업데이트합니다.
-    showToast('운동이 삭제되었습니다.'); // 사용자에게 피드백을 제공합니다.
+    setMarkedDates(getMarkedDates(updatedWorkouts));
+    showToast('운동이 삭제되었습니다.');
   };
 
   // 토스트 메시지를 표시하는 함수
@@ -126,11 +180,11 @@ const CalendarScreen = () => {
     if (Platform.OS === 'android') {
       ToastAndroid.show(message, ToastAndroid.SHORT);
     } else {
-      Alert.alert(message); // iOS의 경우 Alert 사용
+      Alert.alert(message);
     }
   };
 
-  // 날짜에 해당하는 운동 정보를 렌더링하는 함수입니다.
+  // 운동 정보를 렌더링하는 함수
   const renderWorkoutInfo = (date) => {
     const dateString = date.toISOString().split('T')[0];
     const dailyWorkouts = workouts[dateString];
@@ -143,44 +197,23 @@ const CalendarScreen = () => {
           style={styles.workoutItem}
           onPress={() => confirmDeleteWorkout(dateString, index)}
         >
-          <Text style={styles.workoutText}>{`${workout.type}: ${workout.name}, 세트: ${workout.sets}, 횟수: ${workout.reps}`}</Text>
+          <Text style={styles.workoutText}>
+            {`${workout.type}: ${workout.name}, `}
+            {workout.timeBased ? `지속 시간: ${workout.duration}초` : `세트: ${workout.sets}, 횟수: ${workout.reps}`}
+          </Text>
         </TouchableOpacity>
       ));
     }
-  
-    return workoutInfoElements.length > 0 ? workoutInfoElements : <Text style={styles.workoutContent}>운동 계획 없음</Text>;
+
+    return workoutInfoElements.length > 0 ? workoutInfoElements : <Text style={styles.workoutText}>운동 계획 없음</Text>;
   };
 
   // RNPickerSelect의 placeholder 옵션을 정의합니다.
   const placeholder = {
     label: '선택...',
     value: null,
-    color: '#9EA0A4', // Placeholder 텍스트의 색상 (옵션)
+    color: '#9EA0A4', 
   };
-
-  // RNPickerSelect 컴포넌트의 스타일을 정의합니다.
-  const pickerSelectStyles = StyleSheet.create({
-    inputIOS: {
-      fontSize: 16,
-      paddingVertical: 12,
-      paddingHorizontal: 10,
-      borderWidth: 1,
-      borderColor: 'gray',
-      borderRadius: 4,
-      color: 'black',
-      paddingRight: 30, // to ensure the text is never behind the icon
-    },
-    inputAndroid: {
-      fontSize: 16,
-      paddingHorizontal: 10,
-      paddingVertical: 8,
-      borderWidth: 0.5,
-      borderColor: 'purple',
-      borderRadius: 8,
-      color: 'black',
-      paddingRight: 30, // to ensure the text is never behind the icon
-    },
-  });
 
   // 메인 화면 렌더링을 위한 코드
   return (
@@ -194,7 +227,6 @@ const CalendarScreen = () => {
         markedDates={markedDates}
         onMonthChange={(month) => { console.log('변경된 달', month); }}
       />
-
       {/* 운동 선택 및 세부 정보 입력을 위한 모달 */}
       <Modal
         animationType="slide"
@@ -204,41 +236,66 @@ const CalendarScreen = () => {
       >
         <View style={styles.modalView}>
           {/* 운동 종류 선택 */}
-          <Text>운동 종류 선택:</Text>
-          <RNPickerSelect
-            onValueChange={(value) => setWorkoutType(value)}
-            items={Object.keys(workoutTypes).map((type) => ({ label: type, value: type }))}
-            placeholder={placeholder}
-            value={workoutType}
-            style={pickerSelectStyles}
-          />
-          {/* 구체적인 운동 선택 */}
-          <Text>구체적인 운동 선택:</Text>
-          <RNPickerSelect
-            onValueChange={(value) => setSelectedWorkout(value)}
-            items={workoutType ? workoutTypes[workoutType].map((workout) => ({ label: workout, value: workout })) : []}
-            placeholder={placeholder}
-            value={selectedWorkout}
-            disabled={!workoutType}
-            style={pickerSelectStyles}
-          />
-          {/* 세트 및 횟수 입력 필드 */}
-          <TextInput
-            style={styles.input}
-            value={sets}
-            onChangeText={text => setSets(text.replace(/[^0-9]/g, ''))}
-            placeholder="세트"
-            keyboardType="numeric"
-            maxLength={3}
-          />
-          <TextInput
-            style={styles.input}
-            value={reps}
-            onChangeText={text => setReps(text.replace(/[^0-9]/g, ''))}
-            placeholder="횟수"
-            keyboardType="numeric"
-            maxLength={3}
-          />
+        <Text>운동 종류 선택:</Text>
+        <RNPickerSelect
+          onValueChange={(value) => {
+            setWorkoutType(value);
+            setSelectedWorkout(null);
+          }}
+          items={Object.keys(workoutTypes).map((type) => ({ label: type, value: type }))}
+          placeholder={placeholder}
+          value={workoutType}
+          style={pickerSelectStyles}
+        />
+        {/* 구체적인 운동 선택 */}
+        <Text>구체적인 운동 선택:</Text>
+        <RNPickerSelect
+          onValueChange={(value) => {
+            const workoutObj = workoutTypes[workoutType]?.find(workout => workout.name === value);
+            setSelectedWorkout(workoutObj);
+            setTimeBased(workoutObj ? workoutObj.timeBased : false);
+          }}
+          items={workoutType ? workoutTypes[workoutType].map((workout) => ({
+            label: workout.name,
+            value: workout.name,
+          })) : []}
+          placeholder={placeholder}
+          value={selectedWorkout ? selectedWorkout.name : ''}
+          disabled={!workoutType}
+          style={pickerSelectStyles}
+        />
+        {/* 세트 및 횟수 입력 필드 */}
+          {
+            timeBased ? (
+              <TextInput
+                style={styles.input}
+                value={duration}
+                onChangeText={setDuration}
+                placeholder="시간 (초)"
+                keyboardType="numeric"
+                maxLength={3}
+              />
+            ) : (
+              <>
+                <TextInput
+                  style={styles.input}
+                  value={sets}
+                  onChangeText={text => setSets(text.replace(/[^0-9]/g, ''))}
+                  placeholder="세트"
+                  keyboardType="numeric"
+                  maxLength={3}
+                />
+                <TextInput
+                  style={styles.input}
+                  value={reps}
+                  onChangeText={text => setReps(text.replace(/[^0-9]/g, ''))}
+                  placeholder="횟수"
+                  keyboardType="numeric"
+                  maxLength={3}
+                />
+              </>
+            )
+          }
           {/* 저장 및 취소 버튼 */}
           <View style={styles.buttonContainer}>
             <View style={styles.buttonWrapper}>
@@ -250,14 +307,15 @@ const CalendarScreen = () => {
           </View>
         </View>
       </Modal>
-
       {/* 오늘과 내일의 운동 정보를 표시하는 부분 */}
-      <View style={styles.workoutInfo}>
-        <Text style={styles.workoutTitle}>오늘의 운동</Text>
-        <Text style={styles.workoutContent}>{renderWorkoutInfo(new Date())}</Text>
-        <Text style={styles.workoutTitle}>내일의 운동</Text>
-        <Text style={styles.workoutContent}>{renderWorkoutInfo(new Date(new Date().getTime() + 24 * 60 * 60 * 1000))}</Text>
-      </View>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.workoutInfo}>
+          <Text style={styles.workoutTitle}>오늘의 운동</Text>
+          {renderWorkoutInfo(new Date())}
+          <Text style={styles.workoutTitle}>내일의 운동</Text>
+          {renderWorkoutInfo(new Date(new Date().getTime() + 24 * 60 * 60 * 1000))}
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -292,6 +350,21 @@ const styles = StyleSheet.create({
     width: '80%',
     backgroundColor: '#fafafa'
   },
+  workoutItem: {
+    backgroundColor: '#fff', 
+    padding: 5, 
+    borderRadius: 2, 
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'space-between',
+  },
+  workoutText: {
+    fontSize: 16,
+    color: '#333333',
+    flex: 1,
+    flexWrap: 'wrap', 
+    marginBottom: 4,
+  },
   workoutInfo: {
     padding: 20,
     backgroundColor: '#ffffff',
@@ -312,10 +385,6 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     color: '#333333'
   },
-  workoutContent: {
-    fontSize: 16,
-    color: '#333333'
-  },
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
@@ -326,6 +395,7 @@ const styles = StyleSheet.create({
   },
 });
 
+// RNPickerSelect 컴포넌트의 스타일을 정의합니다.
 const pickerSelectStyles = StyleSheet.create({
   inputIOS: {
     fontSize: 16,
